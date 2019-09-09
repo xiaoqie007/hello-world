6c124446fdcf2ccee8cd70084227e8b72f170073
xiaoqie007 <1072907380@qq.com>
2019-09-09 19:29:51 +0800

    pydate_learn9.9

---
+++ selfteaching-python-zby/Learn9.9xiaoqie.py
@@ -0,0 +1,46 @@
+## 2.
+
+"""A researcher has gathered thousands of news articles. But she wants to focus her attention on articles including a specific word. Complete the function below to help her filter her list of articles.
+
+Your function should meet the following criteria
+
+- Do not include documents where the keyword string shows up only as a part of a larger word. For example, if she were looking for the keyword “closed”, you would not include the string “enclosed.” 
+- She does not want you to distinguish upper case from lower case letters. So the phrase “Closed the case.” would be included when the keyword is “closed”
+- Do not let periods or commas affect what is matched. “It is closed.” would be included when the keyword is “closed”. But you can assume there are no other types of punctuation.
+"""
+
+def word_search(doc_list, keyword):
+    """
+    Takes a list of documents (each document is a string) and a keyword. 
+    Returns list of the index values into the original list for all documents 
+    containing the keyword.
+
+    Example:
+    doc_list = ["The Learn Python Challenge Casino.", "They bought a car", "Casinoville"]
+    >>> word_search(doc_list, 'casino')
+    >>> [0]
+    """
+    indices = []
+    for i, doc in enumerate(doc_list):
+        tokens = doc.split()
+        normalized = [token.rstrip(',.').lower() for token in tokens]
+        if keyword.lower() in normalized:
+            indices.append(i)
+    return indices
+
+def multi_word_research(doc_list, keywords):
+    """
+    Takes list of documents (each document is a string) and a list of keywords.  
+    Returns a dictionary where each key is a keyword, and the value is a list of indices
+    (from doc_list) of the documents containing that keyword
+
+    >>> doc_list = ["The Learn Python Challenge Casino.", "They bought a car and a casino", "Casinoville"]
+    >>> keywords = ['casino', 'they']
+    >>> multi_word_search(doc_list, keywords)
+    {'casino': [0, 1], 'they': [1]}
+    """
+    keyword_to_indices = {}
+    for keyword in keywords:
+        keyword_to_indices[keyword] = word_search(doc_list, keyword)
+    return keyword_to_indices
+
